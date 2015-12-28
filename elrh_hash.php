9d950a389274c6aae5fa7413416605567a6e521f
Alois Sečkár <ellrohir@seznam.cz>
2015-12-28 10:26:40 +0100

    Styles cleanup

--- elrh_hash.php
+++ elrh_hash.php
@@ -1,37 +1,24 @@
-<?php
-///////////////////////////////////////////////////////////////////////
-//                                                                   //
-//                      PHP functions MD5 and SH1                    //
-//                       Quick hashing maschine                      //
-//                                                                   //
-//                   Copyright © Ellrohir 2009-2012                  // 
-//                                                                   //
-//                                                                   //
-//    Page Info                                                      //
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-// *  Author:         *  Ellrohir [ ellrohir@seznam.cz ]           * //
-// *  Homepage:       *  http://ellrohir.xf.cz                     * //
-// *  File:           *  elrh_hash.php                             * //
-// *  Purpose:        *  Page for quick on-line string hashing by  * //
-// *                  *  MD5 and SH1 hashing functions             * //
-// *  System Version: *  -                                         * //
-// *  Last Modified:  *  2012-01-02 1845 GMT+1                     * //
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-///////////////////////////////////////////////////////////////////////
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
  <head>
   <meta http-equiv="content-type" content="text/html; charset=windows-1250" />
   <meta name="generator" content="PSPad editor, www.pspad.com" />
   <title>Ellrohir - PHP functions MD5 and SH1</title>
+  <style type="text/css">
+	div { border-style: ridge; width: 400px; padding: 5px; margin: 5px; }
+	div.d1 { background-color: #4682b4; }
+	div.d2 { background-color: #483d8b; }
+	div.d3 { background-color: #db7093; }
+	div.d4 { background-color: #dc143c; }
+	div.d5 { background-color: #2e8b57; }
+	div.d6 { background-color: #008000; }
+  </style>
  </head>
  <body>
   <? if (isset($_GET["hash"])) $hash = $_GET["hash"]; else $hash = 0; ?>
   <h2>&nbsp;&nbsp;1) Hashing using MD5($string)</h2>
   <? if ($hash=="1") { ?>
-  <div style="border-style: ridge; background-color: #4682b4; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d1">
    <table align="center">
     <tr>
      <td><b>$string :</b></td>
@@ -46,7 +33,7 @@
    </table>
   </div>
   <? } ?>
-  <div style="border-style: ridge; background-color: #483d8b; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d2">
    <form action="elrh_hash.php?hash=1" method="post">
     <table align="center">
      <tr>
@@ -65,7 +52,7 @@
   </div>
   <h2>&nbsp;&nbsp;2) Hashing using SHA1($string)</h2>
   <? if ($hash=="2") { ?>
-  <div style="border-style: ridge; background-color: #db7093; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d3">
    <table align="center">
     <tr>
      <td><b>$string :</b></td>
@@ -80,7 +67,7 @@
    </table>
   </div>
   <? } ?>
-  <div style="border-style: ridge; background-color: #dc143c; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d4">
    <form action="elrh_hash.php?hash=2" method="post">
     <table align="center">
      <tr>
@@ -99,7 +86,7 @@
   </div>
   <h2>&nbsp;&nbsp;3) Hashing using SHA1(MD5($string))</h2>
   <? if ($hash=="3") { ?>
-  <div style="border-style: ridge; background-color: #2e8b57; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d5">
    <table align="center">
     <tr>
      <td><b>$string :</b></td>
@@ -114,7 +101,7 @@
    </table>
   </div>
   <? } ?>
-  <div style="border-style: ridge; background-color: #008000; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d6">
    <form action="elrh_hash.php?hash=3" method="post">
     <table align="center">
      <tr>
@@ -132,7 +119,7 @@
    </form>
   </div>
   <p>
-   &nbsp;&nbsp;<font size="2">© 2009-2012 <a href="http://ellrohir.xf.cz">Ellrohir</a><br /></font>
+   &nbsp;&nbsp;<font size="2">© 2009-2015 <a href="http://alois-seckar.cz">Ellrohir</a><br /></font>
    &nbsp;<a href="http://validator.w3.org/check?uri=referer">
     <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
    </a>
--- elrh_rand.php
+++ elrh_rand.php
@@ -1,35 +1,20 @@
-<?php
-///////////////////////////////////////////////////////////////////////
-//                                                                   //
-//                          PHP function RAND                        //
-//                  Random numbers generating maschine               //
-//                                                                   //
-//                   Copyright © Ellrohir 2009-2012                  // 
-//                                                                   //
-//                                                                   //
-//    Page Info                                                      //
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-// *  Author:         *  Ellrohir [ ellrohir@seznam.cz ]           * //
-// *  Homepage:       *  http://ellrohir.xf.cz                     * //
-// *  File:           *  elrh_rand.php                             * //
-// *  Purpose:        *  Quick random numbers generator            * //
-// *  System Version: *  -                                         * //
-// *  Last Modified:  *  2012-01-02 1845 GMT+1                     * //
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-///////////////////////////////////////////////////////////////////////
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
  <head>
   <meta http-equiv="content-type" content="text/html; charset=windows-1250" />
   <meta name="generator" content="PSPad editor, www.pspad.com" />
   <title>Ellrohir - PHP function Rand</title>
+  <style type="text/css">
+	div { border-style: ridge; width: 400px; padding: 5px; margin: 5px; }
+	div.d1 { background-color: #c0c0c0; }
+	div.d2 { background-color: #a0a0a0; }
+  </style>
  </head>
  <body>
   <? if (isset($_GET["rand"])) $rand = 1; else $rand = 0; ?>
   <h2>&nbsp;&nbsp;1) Random numbers generator</h2>
   <? if ($rand=="1") { ?>
-  <div style="border-style: ridge; background-color: #c0c0c0; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d1">
    <table align="center">
     <tr>
      <td><b>Random :</b></td>
@@ -44,7 +29,7 @@
    </table>
   </div>
   <? } ?>
-  <div style="border-style: ridge; background-color: #a0a0a0; width: 400px; padding: 5px; margin: 5px;">
+  <div class="d2">
    <form action="elrh_rand.php?rand=1" method="post">
     <table align="center">
      <tr>
@@ -62,7 +47,7 @@
    </form>
   </div>
   <p>
-   &nbsp;&nbsp;<font size="2">© 2009 <a href="http://ellrohir.xf.cz">Ellrohir</a><br /></font>
+   &nbsp;&nbsp;<font size="2">© 2009-2015 <a href="http://alois-seckar.cz">Ellrohir</a><br /></font>
    &nbsp;<a href="http://validator.w3.org/check?uri=referer">
     <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
    </a>
